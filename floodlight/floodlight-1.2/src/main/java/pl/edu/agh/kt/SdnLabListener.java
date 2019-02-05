ae95b58b125b25c2623887d7ecbe408d00946d14
ZajMar <marcio.zet@gmail.com>
2019-02-06 00:34:35 +0100

    drzewo rozpinajace + liczenie sciezki

--- floodlight/floodlight-1.2/src/main/java/pl/edu/agh/kt/SdnLabListener.java
+++ floodlight/floodlight-1.2/src/main/java/pl/edu/agh/kt/SdnLabListener.java
@@ -2,9 +2,13 @@ package pl.edu.agh.kt;
 
 import static org.easymock.EasyMock.createMock;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 import java.util.List;
 import java.util.HashMap;
@@ -15,7 +19,7 @@ import org.projectfloodlight.openflow.protocol.OFPortDesc;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.OFPort;
-
+import org.python.google.common.collect.ImmutableList;
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
@@ -31,9 +35,14 @@ import net.floodlightcontroller.devicemanager.internal.DeviceManagerImpl;
 import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.linkdiscovery.web.LinksResource;
+import net.floodlightcontroller.routing.BroadcastTree;
 import net.floodlightcontroller.routing.Link;
+import net.floodlightcontroller.routing.PathId;
 import net.floodlightcontroller.topology.ITopologyService;
+import net.floodlightcontroller.topology.NodePortTuple;
+import net.floodlightcontroller.topology.TopologyInstance;
 import net.floodlightcontroller.topology.TopologyManager;
+import net.floodlightcontroller.routing.Path;
 
 import java.util.ArrayList;
 
@@ -41,6 +50,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
+
+
+    public static final int MAX_LINK_WEIGHT = 10000;
+    public static final int MAX_PATH_WEIGHT = Integer.MAX_VALUE - MAX_LINK_WEIGHT - 1;
+    public static final int PATH_CACHE_SIZE = 1000;
 
 	protected IFloodlightProviderService floodlightProvider;
 	protected static Logger logger;
@@ -50,8 +64,9 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 	protected Map<Integer, DatapathId> addrMap = new HashMap<Integer, DatapathId>(); 
 	
 	private Map<Long,DatapathId> nodes = new HashMap<>(); 
+	private Map<DatapathId,Set<Link>> nodesWithLinks= new HashMap<>();
 	boolean firstTime = true;
-    private List<LinkContainer> linksContainer = new ArrayList<>();
+    private Map<Link,Integer> linksContainer = new HashMap<>();
 	
 	@Override
 	public String getName() {
@@ -94,14 +109,14 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		extractor.packetExtract(cntx);
 
 		Object tmpObj = cntx.getStorage().get("net.floodlightcontroller.core.IFloodlightProvider.piPayload");
-		Ethernet eth = (Ethernet)tmpObj;
+		/*Ethernet eth = (Ethernet)tmpObj;
 		ARP arp = (ARP) eth.getPayload();
 		int ipAddr = arp.getTargetProtocolAddress().getInt();
 		
 		DatapathId targetDpId = addrMap.get(ipAddr);// Tu jest ID ostatniego nodea w sieci, potrzebne do Dijkstry
-		logger.info("TargetNodeIP: " + targetDpId.toString()); 		
+		//logger.info("TargetNodeIP: " + targetDpId.toString()); 		
 	
-		
+		*/
 		
 		//TODO LAB 5
 		
@@ -110,15 +125,37 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		Flows.sendPacketOut(sw);
 		if(firstTime==true){
 			firstTime=false;
+			//TUTAJ ROBIE INIT KOSZOTW NA LACZACH
 			topologyInit(allIds);
 			
+		} 
+		//TESTOWE - do usuniecia
+		DatapathId test1 = null,test2 = null;
+		int xd=1;
+		for(DatapathId data : allIds){
+			if(xd==1){
+			test1 = data;
+			}
+			if(xd==4){
+			test2=data;
+			}
+			xd++;
 		}
-		logger.info("MOJEMOJEMOJE"+nodes.toString());
-		logger.info("MOJEJEOEJEO" + linksContainer.toString());
+		logger.info("MOJEMOJEMOJE"+test1.toString()+"ss"+test2.toString());
+		logger.info("MOJEJEOEJEO" + linksContainer.keySet().toString());
+		//TODO DODANIE PRZELICZANIA KOSZTOW NA LINKACH
+		//buildLinkDpidMap(tm.switchService.getAllSwitchDpids(),tm.getSwitchPorts(),tm.linkDiscoveryService.getPortLinks())
+		//TWORZE DRZEWO ROZPINAJACE DIJKSTRY - Trzeba dac zrodlo jako docelowy - czyli to test1 = destnation
+		BroadcastTree tree = dijkstraa(tm.linkDiscoveryService.getSwitchLinks(),test1,linksContainer,true);
 		
 		ArrayList<String> path = new ArrayList<>();
-		
-					
+		logger.info("TEEEEEEEEEST");
+		logger.info(tree.toString());
+		//WYLICZAM SCIEZKE OD test2 do test1 (teraz jest 1-->3 - wazne zeby nie robic src = destiantion ustawione
+		// na drzewie rozpinajacym bo sie wywroci cale
+		Path newroute = buildPath(new PathId(test2, test1), tree);	
+		//TODO przeparsowac newroute na liste Stringow do zmiennej path - mozliwe ze 5min roboty
+		logger.info("PATH"+newroute.toString()); 
 					/*
 					 *TU trzeba wywolac dijkstre ktora wrzuci do listy "path" po kolei id'ki  nodow, (sciezka)
 					 *Do zrobienia:
@@ -268,12 +305,144 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
         for(DatapathId node : allIds){
         	nodes.put(node.getLong(),node);
         }
-        long linkIdIterator = 0;
+        int changeCost = 1;
         for(Link link : tm.linkDiscoveryService.getLinks().keySet()){
-        	linksContainer.add(new LinkContainer(link.getSrc().getLong(),link.getDst().getLong(),linkIdIterator,1L));
-        	linkIdIterator++;
+        	linksContainer.put(link,changeCost);
+        	changeCost++;
         }
 	}
 
+	private BroadcastTree dijkstraa(Map<DatapathId, Set<Link>> links, DatapathId root,
+            Map<Link, Integer> linkCost,
+            boolean isDstRooted) {
+        HashMap<DatapathId, Link> nexthoplinks = new HashMap<DatapathId, Link>();
+        HashMap<DatapathId, Integer> cost = new HashMap<DatapathId, Integer>();
+        int w;
+        logger.info("DIJKSTRA"+root.toString());
+        for (DatapathId node : links.keySet()) {
+            nexthoplinks.put(node, null);
+            cost.put(node, MAX_PATH_WEIGHT);
+            //log.debug("Added max cost to {}", node);
+        }
+
+        HashMap<DatapathId, Boolean> seen = new HashMap<DatapathId, Boolean>();
+        PriorityQueue<NodeDist> nodeq = new PriorityQueue<NodeDist>();
+        nodeq.add(new NodeDist(root, 0));
+        cost.put(root, 0);
+
+        //log.debug("{}", links);
+
+        while (nodeq.peek() != null) {
+            NodeDist n = nodeq.poll();
+            DatapathId cnode = n.getNode();
+            int cdist = n.getDist();
+
+            if (cdist >= MAX_PATH_WEIGHT) break;
+            if (seen.containsKey(cnode)) continue;
+            seen.put(cnode, true);
+
+            //log.debug("cnode {} and links {}", cnode, links.get(cnode));
+            if (links.get(cnode) == null) continue;
+            for (Link link : links.get(cnode)) {
+                DatapathId neighbor;
+
+                if (isDstRooted == true) {
+                    neighbor = link.getSrc();
+                } else {
+                    neighbor = link.getDst();
+                }
+
+                // links directed toward cnode will result in this condition
+                if (neighbor.equals(cnode)) continue;
+
+                if (seen.containsKey(neighbor)) continue;
+
+                if (linkCost == null || linkCost.get(link) == null) {
+                    w = 1;
+                } else {
+                    w = linkCost.get(link);
+                }
+
+                int ndist = cdist + w; // the weight of the link, always 1 in current version of floodlight.
+
+
+                if (ndist < cost.get(neighbor)) {
+                    cost.put(neighbor, ndist);
+                    nexthoplinks.put(neighbor, link);
+                    NodeDist ndTemp = new NodeDist(neighbor, ndist);
+                    // Remove an object that's already in there.
+                    // Note that the comparison is based on only the node id,
+                    // and not node id and distance.
+                    nodeq.remove(ndTemp);
+                    // add the current object to the queue.
+                    nodeq.add(ndTemp);
+                }
+            }
+        }
+
+        BroadcastTree ret = new BroadcastTree(nexthoplinks, cost);
+
+        return ret;
+	}
+	private class NodeDist implements Comparable<NodeDist> {
+        private final DatapathId node;
+        public DatapathId getNode() {
+            return node;
+        }
+
+        private final int dist;
+        public int getDist() {
+            return dist;
+        }
+
+        public NodeDist(DatapathId node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+		@Override
+		public int compareTo(NodeDist o) {
+            if (o.dist == this.dist) {
+                return (int)(this.node.getLong() - o.node.getLong());
+            }
+            return this.dist - o.dist;
+		}
+	}
+
+
+    private Path buildPath(PathId id, BroadcastTree tree) {
+    	
+    	net.floodlightcontroller.core.types.NodePortTuple npt;
+        DatapathId srcId = id.getSrc();
+        DatapathId dstId = id.getDst();
+        //set of NodePortTuples on the route
+        LinkedList<net.floodlightcontroller.core.types.NodePortTuple> sPorts = new LinkedList<net.floodlightcontroller.core.types.NodePortTuple>();
+
+
+        Map<DatapathId, Link> nexthoplinks = tree.getLinks();
+        logger.info("tutajtutautjtj"+nexthoplinks.get(srcId));
+        if ((nexthoplinks != null) && (nexthoplinks.get(srcId) != null)) {
+        	logger.info("tutajtutautjtj");
+            while (!srcId.equals(dstId)) {
+                Link l = nexthoplinks.get(srcId);
+                npt = new net.floodlightcontroller.core.types.NodePortTuple(l.getSrc(), l.getSrcPort());
+                sPorts.addLast(npt);
+                npt = new net.floodlightcontroller.core.types.NodePortTuple(l.getDst(), l.getDstPort());
+                sPorts.addLast(npt);
+                srcId = nexthoplinks.get(srcId).getDst();
+            }
+        }
+        // else, no path exists, and path equals null
+
+        Path result = null;
+        if (sPorts != null && !sPorts.isEmpty()) {
+            result = new Path(id, sPorts);
+
+        }
+
+        logger.trace("buildpath: {}", result);
+        return result;
 }
+}
+
 
